dce18655796b6d22b363d3196379f2757885bb38
Meet Mandaviya <codewithmeet@users.noreply.github.com>
2020-06-29 09:56:00 +0530

    Major  Changes
    PreLoader added and Bugs Fixed

--- js/main.js
+++ js/main.js
@@ -1,22 +1,106 @@
 $(function() {
+
+    // Pre-loader
+    window.onload = function() {
+        $('#pre-loader').fadeOut(1000, function() {
+            $('#pre-loader').remove();
+        });
+        $('body').delay(350).css({
+            'overflow-y': 'visible'
+        });
+    }
+
+    // Init Pointer
+    init_pointer();
+
+    // Init AOS
     AOS.refresh();
     //Animation Intilization 
     AOS.init({
-        duration: 1000,
-        easing: 'ease-out-back',
-    })
+            duration: 1000,
+            easing: 'ease-out-back',
+        })
+        // SMooth Scroll
+    var scroll = new SmoothScroll('.nav-link[href*="#"]');
+    var easeInQuad = new SmoothScroll('[data-easing="easeInQuad"]', { easing: 'easeInQuad' });
 
+
+
+    // Modal 
     $('#post-modal').modal('show');
 
     setTimeout(function() {
         $('#post-modal').modal('hide');
-    }, 5000);
+    }, 3000);
 
 });
 
+// Pointer Starts
+// Init Pointer
+
+
+const ring = document.createElement("div")
+ring.id = "pointer-ring"
+document.body.insertBefore(ring, document.body.children[0])
+
+let mouseX = -100
+let mouseY = -100
+let ringX = -100
+let ringY = -100
+let isHover = false
+let mouseDown = false
+
+const init_pointer = (options) => {
+
+    window.onmousemove = (mouse) => {
+        if (window.innerWidth > 768) {
+
+            mouseX = mouse.clientX
+            mouseY = mouse.clientY
+
+        }
+    }
+
+    window.onmousedown = (mouse) => {
+        mouseDown = true
+    }
+
+    window.onmouseup = (mouse) => {
+        mouseDown = false
+    }
+
+    const trace = (a, b, n) => {
+        return (1 - n) * a + n * b;
+    }
+    window["trace"] = trace
+
+    const getOption = (option) => {
+        return options[option]
+
+    }
+
+    const render = () => {
+        ringX = trace(ringX, mouseX, 0.2)
+        ringY = trace(ringY, mouseY, 0.2)
+
+        if (mouseDown) {
+            ring.style.padding = 10 + "px"
+        } else {
+            ring.style.padding = 15 + "px"
+        }
+
+        ring.style.transform = `translate(${ringX - (mouseDown ? 10 : 15 )}px, ${ringY - (mouseDown ? 10 : 15)}px)`
+
+        requestAnimationFrame(render)
+    }
+    requestAnimationFrame(render)
+}
+
+// POinter end
 
 (function($) {
     "use strict";
+
     // COLOR MODE
     $('.color-mode').click(function() {
         $('body').toggleClass('dark-mode')
@@ -43,7 +127,6 @@ $(function() {
     $('.owl-carousel').owlCarousel({
         items: 1,
         loop: true,
-        margin: 10,
         nav: true,
         autoplay: true,
         autoplayTimeout: 2000,
@@ -53,6 +136,5 @@ $(function() {
     var $hamburger = $(".hamburger");
     $hamburger.on("click", function(e) {
         $hamburger.toggleClass("is-active");
-        // Do something else, like open/close menu
     });
 })(jQuery);
